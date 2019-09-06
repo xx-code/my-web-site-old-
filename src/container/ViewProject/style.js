85dc71504a8486dddec13fd9b3fb30118ab8e7ec
Auguste Junior <akaaugustejunior@gmail.com>
2019-09-06 09:27:19 +0000

    ajout navbar projet view  terminer

--- src/container/ViewProject/style.js
+++ src/container/ViewProject/style.js
@@ -1,6 +1,34 @@
-import { WHITE,
+import { BLACK_SKY,
+         WHITE,
     PRIMARY_COLOR} from '../../config/Color';
 
-export default { 
-
+export default {
+    navbar: {
+        backgroundColor: BLACK_SKY,
+        padding: '14px',
+        boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'
+    },
+    backBtn: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    iconBack: {
+        fontSize: '30px',
+        color: WHITE
+    },
+    backTextContent: {
+        marginLeft: '20px'
+    },
+    backText:{
+        fontSize: '25px',
+        color: WHITE,
+        fontStyle: 'bold'
+    },
+    bar: {
+        width: "40%",
+        backgroundColor: WHITE,
+        height: '1px',
+        marginTop: '-6px'
+    },
 }
